468df5d777a01fb61202f3897a1d93af85919363
lizus <lizus.com@gmail.com>
2020-12-31 15:32:01 +0800

    20201231

--- src/class/Post/QueryPost.php
+++ src/class/Post/QueryPost.php
@@ -1,7 +1,7 @@
 <?php
 namespace LizusContinue\Post;
 
-abstract class QueryPost extends LizusVitara\Post\QueryPost
+abstract class QueryPost extends \LizusVitara\Post\QueryPost
 {
     protected function get_item($pid){
         return compact('pid');
