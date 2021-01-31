70b24931d8ec819b28905fed7d110f12099d9e7c
lizus <lizus.com@gmail.com>
2021-01-31 11:46:09 +0800

    20210131

--- src/action/save_post-post_updated.php
+++ src/action/save_post-post_updated.php
@@ -3,6 +3,7 @@
  * 文章发布的时候，在作者的usermeta中记录该作者最近的更新时间
  */
 add_action( "save_post", function ($pid,$post,$update){
+  if($post->post_type == 'nav_menu_item') return;
   if ($post->post_status=='publish') {
     $user=new \LizusContinue\User\User($post->post_author);
     $user->set('post_updated',current_time('timestamp'));
