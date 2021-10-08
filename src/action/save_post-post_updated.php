3dfdfeda58bc03c35ef4a3d04705bf3e24d91fa9
lizus <lizus.com@gmail.com>
2021-10-08 08:08:18 +0800

    20211008

--- src/action/save_post-post_updated.php
+++ src/action/save_post-post_updated.php
@@ -1,7 +1,9 @@
 <?php
 /**
  * 文章发布的时候，在作者的usermeta中记录该作者最近的更新时间
+ * 该功能应该布署在主题的action中，根据需要重新设定
  */
+/*
 add_action( "save_post", function ($pid,$post,$update){
   if($post->post_type == 'nav_menu_item') return;
   if ($post->post_status=='publish') {
@@ -9,3 +11,4 @@ add_action( "save_post", function ($pid,$post,$update){
     $user->set('post_updated',current_time('timestamp'));
   }
 },10,3);
+*/
