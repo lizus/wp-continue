b174677151d9a45bce78a4190a0b6f44d667c735
lizus <lizus.com@gmail.com>
2024-01-05 03:58:24 +0000

    20240105

--- src/action/admin_head.php
+++ src/action/admin_head.php
@@ -1,10 +1,12 @@
 <?php
-if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) {
+    die();
+}
 /**
  * 后台挂载需要的css样式，用于后台添加的功能
  */
-add_action('admin_head',function (){
-    $arr=[
+add_action('admin_head', function () {
+    $arr = [
         'colorbox',
         'fontello',
         'jquery.smartSelect',
@@ -13,11 +15,12 @@ add_action('admin_head',function (){
     ];
 
     foreach ($arr as $item) {
-        echo '<link rel="stylesheet" href="'.\LizusContinue\get_static_uri().'/css/'.$item.'.css?ver='.\LizusContinue\get_version().'">'."\n";
+        echo '<link rel="stylesheet" href="' . \LizusContinue\get_static_uri() . '/css/' . $item . '.css?ver=' . \LizusContinue\get_version() . '">' . "\n";
     }
 
     /**
      * 解决高版本下colorbox出错
+     * 该js最好从官网下载，因为官网在国内常常无法访问，所以尽量放本地
      */
-    echo '<script src="https://code.jquery.com/jquery-migrate-1.4.1.js" charset="utf-8"></script>'; 
+    //echo '<script src="https://code.jquery.com/jquery-migrate-1.4.1.js" charset="utf-8"></script>'; 
 });
