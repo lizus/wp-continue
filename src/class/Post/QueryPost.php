82d83de27e129c42c9b3da0c0b81461ab04c762e
lizus <lizus.com@gmail.com>
2024-03-01 09:07:54 +0800

    20240301

--- src/class/Post/QueryPost.php
+++ src/class/Post/QueryPost.php
@@ -1,10 +1,28 @@
 <?php
+
 namespace LizusContinue\Post;
 
 class QueryPost extends \LizusVitara\Post\QueryPost
 {
-    protected function get_item($id){
-        $pid=$id;
-        return compact('id','pid');
+    /**
+     * 在cache的key中添加last_changed标识，以便于缓存在文章更新时自动刷新
+     */
+    protected function key()
+    {
+        $query = $this->args;
+        $pts = $query['post_type'];
+        if (is_string($pts)) $pts = [$pts];
+        $pts = array_map(function ($pt) {
+            $last_changed = wp_cache_get_last_changed($pt . '_posts');
+            list($microseconds, $seconds) = explode(' ', $last_changed);
+            return intval($seconds);
+        }, $pts);
+        $pts = max($pts);
+        return parent::key() . ':' . $pts;
+    }
+    protected function get_item($id)
+    {
+        $pid = $id;
+        return compact('id', 'pid');
     }
 }
