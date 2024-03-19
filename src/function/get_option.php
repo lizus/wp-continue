6391950ed8e81e5eb7c2045ebca008b02f3aaa19
lizus <lizus.com@gmail.com>
2024-03-19 17:56:48 +0800

    20240319

--- src/function/get_option.php
+++ src/function/get_option.php
@@ -1,12 +1,14 @@
 <?php
+
 namespace LizusContinue;
 
 /**
  * get_option
  * 用于获取options表中网站定制的设置值
  * @param  mixed $key
- * @return void
+ * @return mixed
  */
-function get_option($key){
+function get_option($key)
+{
     return \get_option(\LizusFunction\v_key($key));
 }
